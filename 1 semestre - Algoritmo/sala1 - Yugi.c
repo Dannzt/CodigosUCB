a5901a567c079b1716f4b8b69af07f1329db05d0
Daniel Ribeiro <111394311+Dannzt@users.noreply.github.com>
2023-08-24 22:41:25 -0300

    Add files via upload

---
+++ 1 semestre - Algoritmo/sala1 - Yugi.c
@@ -0,0 +1,25 @@
+int main() {
+      int x=0;
+//    
+//    for (x=5;x>=;x--){
+//          printf("%d\n",x);
+//     }    
+
+            for (x=1;x<=100;x++){
+            
+                  if(x%5==0){
+                        printf("M5\n");
+                  }else{
+                        printf("%d\n",x);
+                  }
+      
+            
+            
+            
+            }
+      
+      
+      
+
+      return 0;
+}
