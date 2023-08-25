292c2e758d5d8014803400a8f115177deec34745
Daniel Ribeiro <111394311+Dannzt@users.noreply.github.com>
2023-08-24 22:31:06 -0300

    Add files via upload

---
+++ 3 semestre - Estrutura de Dados/sala/sala2-Altura.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+typedef struct{
+      float Peso;
+      int Idade;
+      float Altura;
+}pessoa;
+
+void ImprimePessoa(pessoa P){
+      printf("Idade: %d Peso: %f Altura %.2f\n",P.Idade,P.Peso,P.Altura);
+}
+
+void SetPessoa(pessoa *P, int idade, float peso, float altura){
+      (*P).Idade = idade;
+      P->Peso = peso;
+      P->Altura = altura;
+}
+
+main() {
+      pessoa Rafa;
+      SetPessoa(&Rafa, 15,70.5,1.75);
+      ImprimePessoa(Rafa);
+}
