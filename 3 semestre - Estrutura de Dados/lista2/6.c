f37df29a53251127779d710fd413121a821da77f
Daniel Ribeiro <111394311+Dannzt@users.noreply.github.com>
2023-09-16 12:25:55 -0300

    Add files via upload

---
+++ 3 semestre - Estrutura de Dados/lista2/6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+
+
+	/* - Escreva uma função em C que realize uma busca sequencial em uma matriz bidimensional de inteiros para 
+encontrar um valor específico fornecido pelo usuário. A função deve retornar a posição da primeira ocorrência do valor 
+ou informar que o valor não foi encontrado */
+
+	main() {
+	setlocale(LC_ALL, "Portuguese");
+	int i,n, vet[10]={9,12,5,17,77,137,44,120,20,65};
+	for (i=0;i<10;i++)
+	printf ("%d\t", vet[i]);
+	
+	printf("\n\nQual é o valor que deseja buscar? ");
+	scanf ("%d", &n);
+	
+	for(i=0;i<10;i++){
+		if(vet[i]==n){
+			printf("%d ocorre na posição %d do vetor. \n", n,i);
+			return 0;
+		}
+	}
+}
